d2899597dcbf2fb7f7baaa02a7006763a485cded
Kirill Kotenko <Kirill Kotenko>
2016-01-10 13:55:42 +0300

    some trash

--- Assets/Logic/Scenes/SceneLevel/BoomLogic.cs
+++ Assets/Logic/Scenes/SceneLevel/BoomLogic.cs
@@ -9,12 +9,14 @@ public class BoomLogic : MonoBehaviour
 
     private Renderer _renderer;
     private Color _color;
+    private Vector3 _scale;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    _renderer = this.gameObject.GetComponent<Renderer>();
 	    _color = _renderer.sharedMaterial.color;
+        _scale = _renderer.transform.localScale;
 	}
 	
 	// Update is called once per frame
@@ -24,11 +26,13 @@ public class BoomLogic : MonoBehaviour
 
 	    _times -= Time.deltaTime;
         _renderer.material.color = _color * new Color(1, 1, 1, Mathf.Lerp(0.0f, 1.0f, _times));
+        _renderer.transform.localScale = _scale * Mathf.Lerp(0.0f, 1.0f, _times);
 
 	    if (_times <= 0.0f)
 	    {
 	        _floated = true;
 	        _renderer.material.color = _color * new Color(1, 1, 1, 0);
+            //_renderer.transform.localScale = _scale * new Vector3(1, 1, 1);
 	    }
 	}
 
